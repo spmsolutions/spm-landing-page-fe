c780076438e8d4c4081ce63506165d7ad2bf8df1
sandrappipes <sandra.abago@gmail.com>
2024-08-07 07:19:43 -0400

    Update copy

--- src/pages/index.js
+++ src/pages/index.js
@@ -35,7 +35,7 @@ export default function Home() {
           position: 'relative',
           width: '100%',
           minHeight: '80vh',
-          background: `url(${useBaseUrl('img/homepage/meh.jpg')}) no-repeat center center`,
+          background: `url(${useBaseUrl('img/homepage/hero-image.jpg')}) no-repeat center center`,
           backgroundSize: 'cover',
         }}
       >
@@ -56,15 +56,15 @@ export default function Home() {
           }}
         >
           <div className="content-width">
-            <h1 style={{ fontSize: '3rem', marginBottom: '1rem' }}>Transform Your Business with Expert SEO Solutions</h1>
+            <h1 style={{ fontSize: '3rem', marginBottom: '1rem' }}>Searches into Sales</h1>
             <p style={{ fontSize: '1.5rem', marginBottom: '2rem' }}>
-              SPM drives growth for middle-market manufacturing, logistics, and processing companies.
+            SPM Solutions drives new customers for manufacturing and processing companies.
             </p>
             <a
               href="#get-started"
               style={{
                 padding: '1rem 2rem',
-                fontSize: '1rem',
+                fontSize: '1.2rem',
                 backgroundColor: '#9e8352',
                 border: 'none',
                 borderRadius: '5px',
@@ -73,77 +73,84 @@ export default function Home() {
                 cursor: 'pointer',
               }}
             >
-              Get a FREE Consultation
+              Get a FREE website analysis
             </a>
           </div>
         </div>
       </div>
-
-      {/* blue: #5B77AF */}
-      {/* green: #345541 */}
       <div style={{ paddingTop: "120px", backgroundColor: '#F5F9FF', color: '#5B77AF' }}>
         <ImageAndText
-          title='SEO that CONVERTS'
-          imageUrl='/spm-landing-page-fe/img/homepage/meh2.jpg'
+          title='Dominate Online Presence'
+          imageUrl='/spm-landing-page-fe/img/homepage/seo-image.jpg'
 alt="SPM SEO"
           gray
         >
-          SPM Solutions makes SEO that CONVERTS painless.
+          SPM Solutions is an all-encompassing online marketing company. With our roots in Search Engine Optimization,
+           we have expanded to ensure we provide a comprehensive SEO approach allowing our customers a more comprehensive 
+           and impactful experience. 
           <br /> <br />
-          At SPM Small Business Solutions, our mission is to empower middle-market manufacturing,
-          logistics, and processing companies by enhancing their online visibility and driving
-          sustainable growth. We are committed to delivering tailored SEO services and
-          innovative digital solutions that meet the unique needs of our clients.
-          By leveraging our industry expertise and cutting-edge technology,
-          we strive to create lasting partnerships that foster growth and success for every business we serve.
+          We work with companies with deep partnerships in their industry. Too often online presence gets overlooked 
+          when operating in an industry such as ours. We have deeply researched business-to-business buying 
+          trends within the manufacturing and processing industries. Through this, we identified that online 
+          presence is a key metric to determine organic lead generation and overall close rate. Your partners 
+          are your partners because of mutual respect in each other's authority within your area of expertise. 
+          We bring your earned and deserved authority to the front lines of the online marketplace.
           <br /> <br />
-          You&apos;ll never want to go back!
+          You&apos;ll never look back!
         </ImageAndText>
       </div>
       <div id='get-started' style={{ color: '#345541' }}>
         <CtaCards
-          title='Guaranteed results, or your money back'
-          subtitle="Choose SPM Small Business Solutions and experience the difference our structured process and comprehensive features can make for your business. Let us help you achieve your digital marketing goals and drive sustainable growth in the competitive online landscape."
+          title='Integrity in your product and authority online'
+          subtitle="As your business takes pride in the product you produce, 
+          we take pride in empowering your online presence. We take a technical 
+          and data driven approach to online marketing, allowing you to solidify 
+          the authority you deserve in the online marketplace."
           cards={[
             {
               icon: icons.frontendDevelopment2,
-              text: 'Advanced SEO Tools',
+              text: 'More Organic Clients',
             },
             {
               icon: icons.marketing,
-              text: 'Industry Expertise',
+              text: 'Higher Close Rate',
             },
             {
               icon: icons.chartAnaytics1,
-              text: 'Data-driven Approaches',
+              text: 'Better Visibility',
             },
             {
               icon: icons.marketingSpecialist,
-              text: 'Customized Solutions',
+              text: 'Increased Predictability',
             }
           ]}
         />
       </div>
       <div id='get-started' style={{ color: '#5B77AF', backgroundColor: '#F5F9FF' }}>
         <TextCards
-          title='SEO Services'
-          subtitle="Our goal is to drive your online growth and ensure your business stands out in the competitive digital landscape."
+          title='Services'
+          subtitle="We have standard programs to enhance our partner’s online marketing presence. 
+          Using our research and data backed approach, we produce and execute custom strategies 
+          for each individual business. We acknowledge no two businesses are the same, and that 
+          uniqueness should be reflected in your strategy. With that said, our proprietary process 
+          is standardized across the board and highly repeatable, given the positive results 
+          yielded from it time after time."
           cards={[
             {
-              title: 'On-Page SEO',
-              subtitle: "We enhance your website's content to ensure it is relevant, engaging, and optimized for targeted keywords. We ensure your URLs are clean, descriptive, and SEO-friendly to improve your site's visibility and user experience. Our team creates a strategic internal linking structure to help search engines understand the hierarchy of your content and improve site navigation for users.",
+              title: 'Online Presence',
+              subtitle: "We have comprehensive programs and consulting to bring your website and entire online presence to a dominant status. As shown through our process and values, we take an all-encompassing look at SEO, which influences other aspects of the online marketing strategy. Our programs address the entire picture and intends to massively effect online marketing efforts and results.",
             },
             {
-              title: 'Off-Page SEO',
-              subtitle: "Link Building: We develop high-quality backlinks from reputable websites in your industry to boost your domain authority and search engine rankings. By leveraging social media platforms, we enhance your online presence and drive traffic to your website. Our experts create and publish valuable content on authoritative websites to increase your brand's exposure and generate quality backlinks.",
+              title: 'Lead Generation',
+              subtitle: "We generate leads on your behalf in a number of ways tailored to your unique business needs, taking into account things like typical sales funnels used and your customer’s behavior. This is accomplished through SEO, social media channels, email, physical gifts or tokens, calls, etc. All of our standard programs include advisory expertise on lead generation.",
             },
             {
-              title: 'Technical SEO',
-              subtitle: "We optimize your website's loading speed to improve user experience and search engine rankings. Our team ensures your website is fully responsive and optimized for mobile devices, enhancing usability and search performance. We make sure search engines can efficiently crawl and index your site by optimizing your XML sitemap, robots.txt file, and site architecture.",
+              title: 'SEO Advisory',
+              subtitle: "SPM provides advisory services for those that have in-house website developers or are looking for an outside perspective. We effectively become your in-house SEO project managers, providing data backed recommendations to your team for you to make the ultimate decisions.",
             },
             {
-              title: 'Keyword Research',
-              subtitle: "We analyze your competitors' keyword strategies to identify opportunities for your business. Our team identifies and targets long-tail keywords that are highly relevant to your niche, driving more qualified traffic to your site. We map targeted keywords to specific pages on your website to ensure optimal relevancy and SEO performance."
+              title: 'Website Development',
+              subtitle: "With our in-house development team, we have a wide range of capabilities for web design. We have the ability to accommodate your needs and budget and provide exceptional products regardless. Our programs all include website development but do not encapsulate our full website build or redesign process."
             },
           ]}
         />
@@ -154,23 +161,27 @@ alt="SPM SEO"
         </div>
         <div className="grid-container">
           <div style={gridItemStyle}>
-            <h3>1. Discovery</h3>
-            <h5>Understanding Client Needs and Goals</h5>
+            <h3 style={{marginBottom: '10px'}}>1. Discovery</h3>
+            <h5>Customer, Competitor, and Brand Analysis</h5>
+            <h5>Current SEO status and analysis</h5>
             <img src="/spm-landing-page-fe/img/spm/discovery.jpg" alt="Discovery" style={imageStyle} />
           </div>
           <div style={gridItemStyle}>
-            <h3>2. Strategize</h3>
-            <h5>Crafting Customized SEO Plans</h5>
+            <h3 style={{marginBottom: '10px'}}>2. Strategize</h3>
+            <h5>SEO and Quality Content Generation Strategy</h5>
+            <h5>Focus on Clear Communication Documentation</h5>
             <img src="/spm-landing-page-fe/img/spm/developstrategy.jpg" alt="Strategy Development" style={imageStyle} />
           </div>
           <div style={gridItemStyle}>
-            <h3>3. Implementation</h3>
-            <h5>Executing the Custom SEO Strategy</h5>
+            <h3 style={{marginBottom: '10px'}}>3. Implement</h3>
+            <h5>Website & Copy Development</h5>
+            <h5>Content Generation Assistance </h5>
             <img src="/spm-landing-page-fe/img/spm/implementation.jpg" alt="Implementation" style={imageStyle} />
           </div>
           <div style={gridItemStyle}>
-            <h3>4. Monitor & Report</h3>
-            <h5>Regular Updates & Performance Tracking</h5>
+            <h3 style={{marginBottom: '10px'}}>4. Maintain</h3>
+            <h5>Paid Advertisement</h5>
+            <h5>Continuous SEO Data Analysis</h5>
             <img src="/spm-landing-page-fe/img/spm/reportmonitor.jpg" alt="Monitoring & Reporting" className='image-style' />
           </div>
         </div>
